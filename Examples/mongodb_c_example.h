af840948545efda9fe1164db7f42cf63e6745a20
LE, Duc-Anh <goplan@cloud-e.biz>
2015-06-06 16:40:48 +0700

    CLDEPLUS-7 #time 3h Continued with MongoDbSourceDriver. Modified Options (OptionArgs) of every drivers to become private variables.

---
+++ Examples/mongodb_c_example.h
@@ -0,0 +1,19 @@
+//
+// Created by LE, Duc Anh on 6/6/15.
+//
+
+#ifndef CLOUD_E_CPLUS_MONGODB_C_EXAMPLE_H
+#define CLOUD_E_CPLUS_MONGODB_C_EXAMPLE_H
+
+
+class mongodb_c_example {
+public:
+    mongodb_c_example() = default;
+    virtual ~mongodb_c_example() = default;
+    mongodb_c_example(const mongodb_c_example &srcmongodb_c_example) = default;
+    mongodb_c_example &operator=(const mongodb_c_example &srcmongodb_c_example) = default;
+
+};
+
+
+#endif //CLOUD_E_CPLUS_MONGODB_C_EXAMPLE_H
