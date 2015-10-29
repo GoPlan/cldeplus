eda130e461312fd7e9399eae1f550c38043ee62e
LE, Duc-Anh <goplan@cloud-e.biz>
2015-10-29 14:09:49 +0700

    CLDEPLUS-90 #time 3h - Updated test build - Added global header (cldeplus.h)

---
+++ Source/cldeplus.h
@@ -0,0 +1,12 @@
+//
+// Created by LE, Duc Anh on 10/29/15.
+//
+
+#ifndef CLDEPLUS_CLDEPLUS_H
+#define CLDEPLUS_CLDEPLUS_H
+
+#include "Foundation/Foundation.h"
+#include "Relation/Relation.h"
+#include "Segmentation/Segmentation.h"
+
+#endif //CLDEPLUS_CLDEPLUS_H_H
--- Source/cldeplus.h.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by LE, Duc Anh on 10/29/15.
-//
-
-#ifndef CLDEPLUS_CLDEPLUS_H_H
-#define CLDEPLUS_CLDEPLUS_H_H
-
-#endif //CLDEPLUS_CLDEPLUS_H_H
