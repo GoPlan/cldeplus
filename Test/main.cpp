642d4a5122728c23ea66e1e610aab40a4f3b2066
LE, Duc-Anh <goplan@cloud-e.biz>
2015-11-03 20:17:16 +0700

    CLDEPLUS-90 #time 2h18m - Renamed gtest file extensions from .hpp to .cpp

--- Test/main.cpp
+++ Test/main.cpp
@@ -4,17 +4,6 @@
 
 #include <gtest/gtest.h>
 
-//#include "Type/CreateValue.hpp"
-//#include "Type/ValueComparison.hpp"
-//#include "Segmentation/Segmentation.hpp"
-//#include "Segmentation/Transformation.hpp"
-//#include "Relation/NamedEntity01.hpp"
-//#include "Relation/NamedEntity02.hpp"
-//#include "Store/SQLiteStore.hpp"
-//#include "Store/MySqlStore00.hpp"
-//#include "Store/MySqlStore01.hpp"
-//#include "Store/MySqlStore02.hpp"
-
 int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();;
